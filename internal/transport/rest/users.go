5daabc779865a7bfd6f9ef9e4f5ba28aef2355c5
ilyasich <s9179175122@gmail.com>
2024-02-21 17:12:10 +0400

    add code

--- internal/transport/rest/users.go
+++ internal/transport/rest/users.go
@@ -167,19 +167,30 @@ func (g *Rest) deleteFavorite(ctx *gin.Context) {
 }
 
 
-func generateTokenUser(ctx *gin.Context, login string) string {
-	userJSON, _ := json.Marshal(map[string]string{"login": login})
-	return base64.StdEncoding.EncodeToString(userJSON)
+//Не понимаю как сделатьЖ Реализовать POST запрос:
+//login: Возвращение токена. В теле сообщения передаем логин пользователя. Токен должен быть сформирован из логина пользователя, преобразованного в JSON и закодированного в base64.
+//функция для генерации токена
+func(g *Rest) generateToken(ctx *gin.Context, login string) {
+	userData := loginRequest.User
+	userDataJson, _ := json.Marshal(userData)
+	token := base64.StdEncoding.EncodeToString(userDataJson)
+
+	// Сохранение токена в репозитории
+	g.service.SaveToken(token, userData)
+
+	// Возврат токена пользователю
+	ctx.JSON(http.StatusOK, gin.H{"token": token})
+
 }
 
-
- func(g *Rest) login(ctx *gin.Context) {
+//метод для входа пользователя
+ func(g *Rest) login(ctx *gin.Context, login string) {
 	var user models.LoginRequest
 	if err := ctx.BindJSON(&user); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	token := generateTokenUser(models.Login)
+	token := generateToken(login)//
 	ctx.JSON(http.StatusOK, gin.H{"token": token})
 }
